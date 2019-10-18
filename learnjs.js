cd900da33472035fbb93eb830c94dc327c9a7c87
Simon Thor <simon11.thor@hotmail.se>
2019-10-18 10:04:36 +0200

    Wrote some test JS code

---
+++ learnjs.js
@@ -0,0 +1,20 @@
+/*
+Test file for learning Javascript.
+Author: Simon Thor
+Year: 2019
+ */
+//import math from ;
+
+function f (x){
+    return math.sin(x);
+}
+
+var a = [1, 2, 3, 'hello', 'ufu', true, false];
+a.pop();
+a.push(4);
+console.log(a);
+for(let i=0; i<a.length; i++){
+    console.log(a[i]);
+}
+
+//console.log(f(10));
