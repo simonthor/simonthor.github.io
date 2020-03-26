d6f0f5ad59211a0b593b013b7e7ca334be95f545
Simon Thor <simon11.thor@hotmail.se>
2020-03-26 01:56:47 +0100

    Added test file (in gitignore)

---
+++ src/pages/test.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import React from 'react';
+import Planet from './solar-system';
+
+export default function Test (props) {
+    /*
+    let x = styled(()=> (Planet.sattr())`
+        
+    `;
+
+     */
+}
