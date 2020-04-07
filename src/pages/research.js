e444f3d24fecfe97a81563622e2d2c78c2905a4e
Simon Thor <simon11.thor@hotmail.se>
2020-04-07 22:39:22 +0200

    added MIT to research.js

--- src/pages/research.js
+++ src/pages/research.js
@@ -26,6 +26,18 @@ export default function Research() {
                 I also presented at two poster sessions: the Swedish national high school science fair (where I proceeded to the final),
                 and the "Svenska Rymdforskares Samarbetsgrupp" conference (a conference for space researchers in Sweden), both in 2019.
             </p>
+            <h2>Debunking Quark and Gluon Jet Definitions</h2>
+            <p>
+                During the summer of 2018, I conducted research at Massachusetts Institute of Technology as part of the Research Science Institute summer program.
+                Research Science Institute is an international fully paid research summer program worth approximately 8000 USD with over 3000 annual applicants to the 80 available spots,
+                out of which two are reserved for Swedish students.
+                <br/>
+                During this program, I conducted research at the Center for Theoretical Physics on particle physics.
+                I studied quark and gluon jets by simulating high energy particle collisions using the Pythia package with <code>C++</code>.
+                I then analyzed the data using <code>Python</code> to identify the difference between quark and gluon jets.
+                My main goal was to determine whether one of the definitions used for quark and gluon jets is useful.
+                I then managed to show that this definition was in fact not operational.
+            </p>
         </>
     );
 }
