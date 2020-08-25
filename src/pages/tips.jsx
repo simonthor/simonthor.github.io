759065d3caef3f6f6a230595646a5e2be8f90b1a
Simon Thor <simon11.thor@hotmail.se>
2020-08-25 16:28:32 +0200

    Added better text for options instead of "all". Added message if no options were found

--- src/pages/tips.jsx
+++ src/pages/tips.jsx
@@ -39,17 +39,17 @@ export default class Tips extends React.Component {
 
         this.Navigator = Navigator;
         this.navWidgets = navWidgets;
-        this.state = {search: '', season: 'all', subject: 'all', type: 'all', age: undefined, archive: false, sortedTips: tips};
+        this.state = {search: '', season: 'season', subject: 'subject', type: 'type', age: undefined, archive: false, sortedTips: tips};
     }
 
     static conditionsFullFilled(tip, criteria) {
-        if (!(tip['season'] === criteria['season'] || criteria['season'] === 'all' || tip['season'] === 'all')) {
+        if (!(tip['season'] === criteria['season'] || criteria['season'] === 'season' || tip['season'] === 'season')) {
             return false;
         }
-        else if (!(tip['type'].includes(criteria['type']) || criteria['type'] === 'all')) {
+        else if (!(tip['type'].includes(criteria['type']) || criteria['type'] === 'type')) {
             return false;
         }
-        else if (!(tip['subject'] === criteria['subject'] || criteria['subject'] === 'all')) {
+        else if (!(tip['subject'] === criteria['subject'] || criteria['subject'] === 'subject' || tip['subject'] === 'STEM')) {
             return false;
         }
         else if (!(!criteria['age'] || !tip['academic level'].hasOwnProperty('age') ||
@@ -89,14 +89,14 @@ export default class Tips extends React.Component {
                         Search
                     </navWidgets.searchButton>
                     <navWidgets.season id="season" onChange={this.getInputChange}>
-                        <option value="all">all</option>
+                        <option value="season">season</option>
                         <option value="spring">spring</option>
                         <option value="summer">summer</option>
                         <option value="autumn">autumn</option>
                         <option value="winter">winter</option>
                     </navWidgets.season>
                     <navWidgets.subject id="subject" onChange={this.getInputChange}>
-                        <option value="all">all</option>
+                        <option value="subject">subject</option>
                         <option value="physics">physics</option>
                         <option value="programming">programming</option>
                         <option value="astronomy">astronomy</option>
@@ -105,7 +105,7 @@ export default class Tips extends React.Component {
                         <option value="biology">biology</option>
                     </navWidgets.subject>
                     <navWidgets.type id="type" onChange={this.getInputChange}>
-                        <option value="all">all</option>
+                        <option value="type">type</option>
                         <option value="camp">camp</option>
                         <option value="association">association</option>
                         <option value="competition">competition</option>
@@ -121,12 +121,17 @@ export default class Tips extends React.Component {
                     </navWidgets.archive>
                 </this.Navigator>
                 {/*Renders all tips that has been sorted out using getTips.*/}
-                {this.state.sortedTips.map((tip)=>(
-                    <Collapsible title={tip.name} key={tip.name}>
-                        {tip.links.map((link)=>(<a style={{marginRight:'1rem'}} href={link} key={link}>Link</a>))}
-                        <p>{tip.info}</p>
-                    </Collapsible>
-                ))}
+                {
+                    this.state.sortedTips.length > 0 ?
+                     (this.state.sortedTips.map((tip)=>(
+                        <Collapsible title={tip.name} key={tip.name}>
+                            {tip.links.map((link)=>(<a style={{marginRight:'1rem'}} href={link} key={link}>Link</a>))}
+                            <p>{tip.info}</p>
+                        </Collapsible>
+                    ))) :
+                    <p style={{textAlign: 'center', fontSize: '1.5rem'}}>Sorry, no tips in this list matched your requirements :(</p>
+                }
+
                 <p style={{textAlign: 'center', fontSize: '0.7rem'}}>
                     Disclaimer: While I do try to keep this site updated, the information here could still be outdated or incorrect.
                 </p>
